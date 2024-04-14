1e5ea2a915fd072f7527582f0fcd674714bd73e5
rahulbera9386 <rahulberastd2002@gmail.com>
2024-04-14 14:42:04 +0530

    Created Project Structure And Added Navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import react from "react"
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Projects from './Pages/Projects';
+import DashBoard from './Pages/DashBoard';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import Header from "./Components/Header";
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
