9d2fc3792fe5046dd6001de9230e8d867fd03988
rahulbera9386 <rahulberastd2002@gmail.com>
2024-04-16 13:48:30 +0530

    Added SignIn Api Route

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,26 +1,59 @@
 import user from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import errorHandler from "../utills/error.js";
-const signUp=async(req,res,next)=>{
-  try{
-    const {username,email,password}=req.body;
-    if(!username || !email || !password || username==="" || email==="" || password==="")
-    {
-      next(errorHandler(400,"All Fields Are Required"))
+import jwt from "jsonwebtoken"
+const signUp = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password || username === "" || email === "" || password === "") {
+      next(errorHandler(400, "All Fields Are Required"))
     }
-    const hashedPassword=await bcrypt.hashSync(password,10);
-    const newUser=new user({
-      username:username,
-      email:email,
-      password:hashedPassword
+    const hashedPassword = await bcrypt.hashSync(password, 10);
+    const newUser = new user({
+      username: username,
+      email: email,
+      password: hashedPassword
     });
     newUser.save();
     res.json("SignUp SuccessFull")
   }
-  catch(error)
-  {
+  catch (error) {
     next(error);
   }
 
 }
-export default signUp;
+
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  try {
+    const validUser = await user.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, 'Invalid password'));
+    }
+    const token = jwt.sign(
+      { id: validUser._id },
+      process.env.JWT_SECRET
+    );
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res.status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { signin, signUp }
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,11 @@
 import express from "express"
+import { signin, signUp } from "../controller/auth.controller.js";
 
-import signUp from "../controller/auth.controller.js";
+
+
 
 const router=express.Router();
 
-router.post("/sign-up",signUp)
+router.post("/sign-up",signUp);
+router.post("/sign-in",signin)
 export default router;
